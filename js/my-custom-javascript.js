43c058ada5bdd07ce35ada11a7bf308a8d12a475
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-02-02 14:05:35 -0600

    adding selector lessson

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,40 @@
 "use strict";
-
+/*
 $(document).ready(function() {
     alert( 'The DOM has finished loading!' );
 });
+
+ */
+
+// ==========ID Selectors
+/*
+let contents = $('#main-heading').html();
+alert(contents);
+
+ */
+
+
+
+
+
+// ==========Class Selectors
+/*
+$(document).ready(function (){
+    $(".codeup").css("border", "solid red 1px");
+});
+*/
+
+
+// =========Element Selectors
+
+$(document).ready(function(){
+    $('li').css('font-size', '20px');
+    $('h1, p, li').css("background", "lightblue")
+    let headingAlert = $('h1').html();
+    alert(headingAlert);
+});
+
+
+
+//===========Multiple Selectors
+
