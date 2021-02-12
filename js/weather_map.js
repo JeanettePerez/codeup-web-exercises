fd78161e22855436eea6d9ed98bd245baba610aa
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-02-11 19:20:55 -0600

    adding new functions for the map

--- js/weather_map.js
+++ js/weather_map.js
@@ -28,14 +28,17 @@
 				let humidity = data.daily[i].humidity;
 				let wind = data.daily[i].wind_speed;
 				let pressure = data.daily[i].pressure;
+				let icon = data.daily[i].weather[0].icon;
 
 				let htmlItem = "<div class='card' style='width: 18rem;'>"
 				htmlItem += "<div class='card-header text-center'>" +  dateEnUS + "</div>"
 
 				htmlItem += "<ul class='list-group list-group-flush'>"
-				htmlItem += "<li class='list-group-item'>"  + minTemp + "°F"
+				htmlItem += "<li class='list-group-item text-center'>"  + minTemp + "°F"
 					+ " / "
 					+ maxTemp + "°F"
+					+ "<br>"
+						+ "<img src='http://openweathermap.org/img/w/" + icon + ".png'>"
 
 					+ "</li>"
 				htmlItem += "<li class='list-group-item'>" 	+ "Description: "
@@ -53,16 +56,22 @@
 				htmlItem += "</ul>"
 				htmlItem += "</div>"
 				html += htmlItem
-				$(".container").html(html);
+				$(".forecast").html(html);
 				console.log(data.daily[i]);
+				console.log(data.lat)
 			}
 
-
+			// function coordinates() {
+			// 	let latitude = data.lat;
+			// 	let longitude = data.lon;
+			// }
 
 
 			// console log for all the data i am going to need to extract and put into the card
 
 			console.log(data);
+			console.log(data.lat);
+			console.log(data.lon);
 			console.log(data.current.dt);
 			console.log(data.daily[0]);
 			console.log(data.daily[0].temp.min);
@@ -80,10 +89,38 @@
 			center: [-98.61,29.49], // starting position [lng, lat]
 			zoom: 10// starting zoom
 		});
-		map.addControl(new mapboxgl.NavigationControl());
-		var marker = new mapboxgl.Marker()
+
+		// var marker = new mapboxgl.Marker()
+		// 	.setLngLat([-98.61,29.49])
+		// 	.addTo(map);
+
+		var marker = new mapboxgl.Marker({
+			draggable: true
+		})
 			.setLngLat([-98.61,29.49])
 			.addTo(map);
+
+		function onDragEnd() {
+			var lngLat = marker.getLngLat();
+			let lng = data.lon;
+			let lat = data.lat;
+			coordinates.style.display = 'block';
+			coordinates.innerHTML =
+				'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+		}
+
+		marker.on('dragend', onDragEnd);
+		var geocoder = new MapboxGeocoder({
+			accessToken: MAPBOX_TOKEN, // mapboxgl.accessToken,
+			marker: {
+				color: 'orange'
+			},
+			mapboxgl: mapboxgl
+		});
+
+		map.addControl(geocoder);
+		map.addControl(new mapboxgl.NavigationControl());
+
 
     }) // document.ready closing tag
 })();
